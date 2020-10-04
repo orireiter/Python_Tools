cb4588a9eb3f41a3dc531c618cdf4f184315e78e
orireiter <60241083+orireiter@users.noreply.github.com>
2020-10-04 19:01:42 +0300

    added logger and threader decorators

---
+++ decorators.py
@@ -0,0 +1,45 @@
+import threading, logging
+from datetime import datetime
+from functools import wraps
+
+
+def threader(func):
+
+    @wraps(func)
+    def run(*args, **kwargs):
+        thread = threading.Thread(target=func, args=args, kwargs=kwargs)
+        thread.start()
+        return thread
+    return run
+
+def logger(func):
+
+    @wraps(func)
+    def log_this(*args,**kwargs):
+        print(func.__name__)
+        logging.basicConfig(filename="./basic_log.txt", level=logging.DEBUG)
+        logging.info(f"{datetime.now()} attempt to run function named {func.__name__}")
+        func()
+
+    return log_this
+
+
+
+
+
+@logger
+@threader
+def abc():
+    for x in range(10):
+        print("abd")
+
+
+@logger
+@threader
+def nums():
+    for x in range(10):
+        print(123)
+
+
+ori = abc()
+koren = nums()
