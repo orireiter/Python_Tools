8259a3eff6a5b222ec890bc500f2ce8cd4ed9f48
orireiter <60241083+orireiter@users.noreply.github.com>
2020-10-19 18:26:48 +0300

    added function that helps parse rabbitmq messages back to jsons

--- extra_tools.py
+++ extra_tools.py
@@ -28,3 +28,9 @@ def get_conf(*key_list):
     except KeyError:
         return "ERROR: one of the keys given does NOT exist"
     return conf
+
+def fix_json_quotings(string):
+    from json import loads
+    fixed_string = string.replace("'", '"')
+    fixed_json = loads(fixed_string)
+    return(fixed_json)
