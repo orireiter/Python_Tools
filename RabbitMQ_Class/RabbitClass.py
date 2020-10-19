46cfd380db189b84b1a32b0717257bd76540870f
orireiter <60241083+orireiter@users.noreply.github.com>
2020-10-19 11:28:48 +0300

    started documenting rabbit lcass

--- RabbitMQ_Class/RabbitClass.py
+++ RabbitMQ_Class/RabbitClass.py
@@ -1,6 +1,66 @@
 import pika, uuid
 
 class Rabbit:
+      '''
+            A class to represent a connection to RabbitMQ server. 
+            Allowing easier use of publishing and consuming messages with the same object, giving the object modularity.
+      
+            Attributes
+            ----------
+            rabbit_parameters: pika.ConnectionParameters
+                  An object containing parameters such as hostname
+                  of rabbit server or credentials to connect.
+                  These are defined upon creating an object and are 
+                  used when sending, receiving messages.
+            connection: pika.BlockingConnection object
+                  Upon creation of this object with the 
+                  use of the parameters above, 
+                  a connection to RabbitMQ server is made.
+                  Allowing to create a channel.
+            channel: connection.channel
+                  Upon execution, the channel can be used to 
+                  declare/delete queues, publish/consume messages and more.
+            channel.qos:
+                  Decides how theis object will consume messages in temrs of quality.
+                  IN THIS CASE it is used to tell the consumer to take 1 message at a time.
+                  As opposed to taking 10 messages messages from the server at once, and 
+                  still dealing with them one message at a time.
+            callback_queue: str
+                  The name of an auto-genereated queue, which is also 
+                  declared to the server upon init, that will be used if 
+                  the object executes send_n_receive.
+                  Meaning - The object needs to have a private queue to get an answer from.
+            channel.basic_consume:
+                  Upon execution it will define the properties of the consumption
+                  of the private queue (for use of send_n_receive).
+                  When purely consuming without using the callback_queue, 
+                  this will overridden and defined again with the relevant queue.
+            msg_id: str(uuid4)
+                  When using send_n_receive(), the message send is 
+                  tagged with this property, as well as this object,
+                  so when this object awaits an answer (which will be tagged the same)
+                  it will know when the dedicated answer arrives.
+            response: None/str
+                  (For the use of send_n_receive) This attribute = None 
+                  until an answer with the right id arrives. 
+                  And then response = body of that message.
+            
+            Methods 
+            -------
+            declare_queue(queue_name: str, passive=False, durable=False, exclusive=False, auto_delete=False, arguments=None):
+                  Declares a queue to the server to be used later on.
+            declare_exhange(exchange_name: str, exchange_type='direct', passive=False, durable=False, auto_delete=False, internal=False, arguments=None):
+                  Declares an exchange to the server to be used later on.
+            close_connection:
+                  Closes the connection to the server.
+            check_corr_id:
+                  Compares the ID given to the message sent, 
+                  and the one of message answered.
+                  If they're the same, self.response=answer's body.
+            send_one:
+                  Send one message to the server (doesn't wait for any response).
+            consume_one
+      '''
       def __init__(self,host='localhost', callback_queue='',credentials = None):
             if credentials == None:
                   self.rabbit_parameters = pika.ConnectionParameters(host)
@@ -16,7 +76,7 @@ class Rabbit:
             self.channel.basic_qos(prefetch_count=1)
             
             # declaring queue for returning answers 
-            queue_default_name = self.channel.queue_declare(queue=callback_queue, exclusive=True)
+            queue_default_name = self.channel.queue_declare(queue=callback_queue, exclusive=True, auto_delete=True)
             self.callback_queue = queue_default_name.method.queue
 
             # creating basic consume basic consume for returning answers
@@ -28,14 +88,14 @@ class Rabbit:
             
             
 
-      def declare_queue(self, queue_name, passive=False, durable=False, exclusive=False, auto_delete=False, arguments=None):
+      def declare_queue(self, queue_name: str, passive=False, durable=False, exclusive=False, auto_delete=False, arguments=None):
             self.channel.queue_declare(queue=queue_name, 
                   passive=passive, 
                   durable=durable, 
                   exclusive=exclusive, 
                   auto_delete=auto_delete, 
                   arguments=arguments)
-      def declare_exchange(self, exchange_name, exchange_type='direct', passive=False, durable=False, auto_delete=False, internal=False, arguments=None):
+      def declare_exchange(self, exchange_name: str, exchange_type='direct', passive=False, durable=False, auto_delete=False, internal=False, arguments=None):
             self.channel.exchange_declare(exchange=exchange_name, 
                   exchange_type=exchange_type, 
                   passive=passive, 
@@ -182,7 +242,6 @@ class Rabbit:
                   
             self.channel.basic_consume(queue=queue_name, on_message_callback=callback)
             self.channel.start_consuming()
-
 
 
 '''
